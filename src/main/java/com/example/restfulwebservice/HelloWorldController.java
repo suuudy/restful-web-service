9eed10869a3cb150821f541a7081fce3c341ddab
suuudy <ykjs47@naver.com>
2023-11-13 20:24:58 +0900

    section1 commit

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // 우리가 작성할 API : GET 방식의 메소드 형태
+    // 지정하고자 하는 URI : /hello-world (호출되는 이름 -> endpoint) 라는 이름으로 지정할 예정
+    // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s",name));
+    }
+}
